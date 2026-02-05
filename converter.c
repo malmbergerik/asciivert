53f5aa823deea30c9beda4a29cd3764b25a72005
malmbergerik <erikmalmberg45@gmail.com>
2026-02-05 19:54:07 +0100

    Started working on binary search

--- converter.c
+++ converter.c
@@ -6,7 +6,43 @@ char intToAscii(int i){
     return ascii[i];
 }
 
-float rgbToBWScale(float red, float green, float blue){
+char getAsciiFromLevel(float level, char arr[], int n){
+    
+    //If no value return blank
+    if(level ==0){
+        return ' ';
+    }
+
+    //As many steps
+    float steps = 1.0/n;
+    printf("Test \f",steps);
+
+    //Binary Search for value
+    int low = 0;
+    int high = n -1; 
+
+    while (low <=high){
+        int mid = low + (high-low)/2;
+        float current_value = steps*mid;
+
+        printf("Current: %f target: %f\n\n",current_value,0.912059);
+
+        if(current_value==level)
+            return mid;
+        if(current_value<level)
+            low = mid+1;
+        else
+            high = mid-1;
+        
+        //printf("Low: %i High: %i\n\n",low,high);
+
+    }
+    
+    return arr[low];
+}
+
+
+float rgbToBWLevel(float red, float green, float blue){
     
     float rWeight = 0.299;
     float gWeight = 0.587;
@@ -26,7 +62,9 @@ int main(void){
     char charact = intToAscii(0);
     printf("Hello World\n");
     printf("This ascii %c \n", charact);
-    float level = rgbToBWScale(180,255,255);
+    float level = rgbToBWLevel(180,255,255);
+    char test = getAsciiFromLevel(level, ascii, 70);
+    printf("This is a test %c",test);
     printf("This level shold be 1 : %f", level);
     return 0;
 }
