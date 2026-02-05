9b21457b4851e02254b51267f76ae5d23b0db0f3
malmbergerik <erikmalmberg45@gmail.com>
2026-02-05 18:06:31 +0100

    int_to_ascii func

--- converter.c
+++ converter.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+char ascii[] = 
+        "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; 
+ 
+char int_to_ascii(int i){
+    return ascii[i];
+}
 
 int main(void){
 
-    char ascii[] = 
-        "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; 
-    
+   
     for(int i=0; i<69; i++)
     {
         printf("Here is the ascii: %c\n", ascii[i]);
     }
+    char charact = int_to_ascii(0);
     printf("Hello World\n");
+    printf("This ascii %c", charact);
     return 0;
 }
