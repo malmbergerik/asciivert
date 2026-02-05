440c4af1d38385ffa6dc8e68dcb121bdc30ac59a
malmbergerik <erikmalmberg45@gmail.com>
2026-02-05 17:53:18 +0100

    Added the ascii and a loop for printing out each char

--- converter.c
+++ converter.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
 
+    char ascii[] = 
+        "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; 
     
+    for(int i=0; i<69; i++)
+    {
+        printf("Here is the ascii: %c\n", ascii[i]);
+    }
     printf("Hello World\n");
     return 0;
 }
