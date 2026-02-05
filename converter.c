21501c7adac0ba7ee47d693f5cafa96964313a24
malmbergerik <erikmalmberg45@gmail.com>
2026-02-05 19:18:08 +0100

    rgbToBwScale function

--- converter.c
+++ converter.c
@@ -2,19 +2,31 @@
 char ascii[] = 
         "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; 
  
-char int_to_ascii(int i){
+char intToAscii(int i){
     return ascii[i];
 }
 
-int main(void){
+float rgbToBWScale(float red, float green, float blue){
+    
+    float rWeight = 0.299;
+    float gWeight = 0.587;
+    float bWeight = 0.114;
 
-   
+    float level = rWeight*(red/255.0) + gWeight*(green/255.0) + bWeight*(blue/255.0);
+
+    return level;
+
+}
+
+int main(void){   
     for(int i=0; i<69; i++)
     {
         printf("Here is the ascii: %c\n", ascii[i]);
     }
-    char charact = int_to_ascii(0);
+    char charact = intToAscii(0);
     printf("Hello World\n");
-    printf("This ascii %c", charact);
+    printf("This ascii %c \n", charact);
+    float level = rgbToBWScale(180,255,255);
+    printf("This level shold be 1 : %f", level);
     return 0;
 }
